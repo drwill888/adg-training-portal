38042278a317e5ed6c8706325df1196d39679bd8
drwill888 <meier.will@gmail.com>
2026-03-20 14:56:58 -0400

    Add files via upload

--- pages/_document.js
+++ pages/_document.js
@@ -1,19 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+// pages/_document.js
+// Adds Tailwind CSS CDN globally to all pages
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,400;0,600;0,700;1,400&family=Outfit:wght@300;400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
+        <script src="https://cdn.tailwindcss.com"></script>
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
